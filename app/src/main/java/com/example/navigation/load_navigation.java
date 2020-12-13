29afda22a6f8b8360c2bc27da6c98f7f2f516505
Tenykim1109 <lona573@gmail.com>
2020-12-13 23:11:09 +0900

    UI 수정
    화살표 방향 수정

--- app/src/main/java/com/example/navigation/load_navigation.java
+++ app/src/main/java/com/example/navigation/load_navigation.java
@@ -48,7 +48,7 @@ public class load_navigation extends AppCompatActivity implements BeaconConsumer
         setContentView(R.layout.activity_load_navigation);
 
         Intent intent = getIntent(); //인식한 비콘에 대한 route 정보담은 객체 받기
-        imageView = (ImageView)findViewById(R.id.imageView);
+        imageView = (ImageView)findViewById(R.id.path_image);
 
         beacon = (Current_beacon) intent.getSerializableExtra("beacon_obj"); //select_destination에서 수신한 비콘에 대한 정보 받기
         beaconManager = BeaconManager.getInstanceForApplication(this);
@@ -162,9 +162,14 @@ public class load_navigation extends AppCompatActivity implements BeaconConsumer
             @Override
             public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) { //비콘이 감지되었을때 실행되는 함수
                 if (beacons.size() > 0) {
-                    if(beacons.iterator().next().getDistance()>=1.0 && beacons.iterator().next().getDistance()<=2.5) { //비콘 인식 거리는 1미터에서 1.5미터
+                    if(beacons.iterator().next().getDistance()>=0.25 && beacons.iterator().next().getDistance()<=4.5) { //비콘 인식 거리는 1미터에서 1.5미터
                         Log.d("beacon_uuid", beacons.iterator().next().getId3().toString());
                         load_guide(beacons.iterator().next().getId3().toString(), beacon);
+                        try {
+                            Thread.sleep(3000); // 비콘 중복 인식 방지를 위해 한번 인식했을 경우 3초간 sleep
+                        } catch(InterruptedException e) {
+                            e.printStackTrace();
+                        }
                     }
 //                    beaconList.clear();
                 }
@@ -181,6 +186,7 @@ public class load_navigation extends AppCompatActivity implements BeaconConsumer
 
     private void load_guide(String UUID, Current_beacon beacon) {
         Intent intent = new Intent(load_navigation.this, arrival_info.class);
+        String dest = beacon.getNavigation().get(UUID); // 경로 메시지
         Log.d("beacon_minorID", UUID);
 //        FirebaseFirestore db = FirebaseFirestore.getInstance();
         /*for (Map.Entry<String, String> entry : beacon.getNavigation().entrySet()) {
@@ -196,22 +202,28 @@ public class load_navigation extends AppCompatActivity implements BeaconConsumer
             }
         }*/
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            ttsGreater21(beacon.getNavigation().get(UUID));
-        } else {
-            ttsUnder20(beacon.getNavigation().get(UUID));
-        }
-
-        if (beacon.getNavigation().get(UUID).contains("전진")) {
+        if (dest.contains("전진")) {
+            Log.d("beacon_dest", "1");
             imageView.setImageResource(R.drawable.north);
-        } else if (beacon.getNavigation().get(UUID).contains("좌회전") || beacon.getNavigation().get(UUID).contains("좌측")) {
-            imageView.setImageResource(R.drawable.east);
-        } else if (beacon.getNavigation().get(UUID).contains("우회전") || beacon.getNavigation().get(UUID).contains("우측")) {
+        } else if (dest.contains("좌회전") || beacon.getNavigation().get(UUID).contains("좌측")) {
+            Log.d("beacon_dest", "2");
             imageView.setImageResource(R.drawable.west);
-        } else if(beacon.getNavigation().get(UUID).contains("목적지")) {
+        } else if (dest.contains("우회전") || beacon.getNavigation().get(UUID).contains("우측")) {
+            Log.d("beacon_dest", "3");
+            imageView.setImageResource(R.drawable.east);
+        } else if(dest.contains("목적지")) {
+            Log.d("beacon_dest", "4");
             startActivity(intent); //arrival_info 화면으로 이동
         } else {
+            Log.d("beacon_dest", "5");
             imageView.setImageResource(R.drawable.south);
         }
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            ttsGreater21(dest);
+        } else {
+            ttsUnder20(dest);
+        }
+
     }
 }
